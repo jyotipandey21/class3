6c080428686009cedbe5e22c453f3b1ad51ec8ee
jyotipandey21 <21jyotipandey21@users.noreply.github.com>
2020-08-24 16:24:45 -0500

    firstjava_commit

---
+++ src/may4/splllit.java
@@ -0,0 +1,187 @@
+package may4;
+ import java.util.Scanner;
+
+public class splllit {
+
+	public static void main(String[] args) {
+		
+		
+		String doll="abseadefaghieajkl";
+		String[] doll1=doll.split("e");
+		
+			//System.out.println(doll1[4]);
+		System.out.println(doll1[0]);
+			  System.out.println(doll1[1]);
+		System.out.println(doll1[2]);
+		//System.out.println(doll1[3]);
+	
+		//	System.out.println(doll1[4]);
+
+		
+		//for(int i=2; i<=100; i=i+1){
+		//if ( i % 2 == 0 ) {
+			
+			//System.out.println(i);
+		//}
+		
+//		String area="$23";
+//		String area1= "$25";
+//		String area2="$45";
+//		String area3="$65";
+		// we are doing split function;
+		String dollar="50,34,36,56,27";
+		
+		String[] dolla1r=dollar.split("3");
+		
+		System.out.println(dolla1r[0]);
+		System.out.println(dolla1r[1]);
+		//System.out.println(dolla1r[2]);
+	//	System.out.println(dolla1r[03]);System.out.println(dolla1r[0]);
+		
+//		System.out.println("hello");
+//		String name1=" ma ";
+//		String name3=" ma ";
+//		String  age3= "324";
+//		
+//		System.out.println(name1 +  name3);
+//		System.out.println(name1.length());
+//		
+		//System.out.println("what is your plan today.");
+		
+		//System.out.println(name3.concat("she is bossy " + age));
+		////System.out.println(name3.indexOf("maaa"));
+		//System.out.println("how are you?");
+		//System.out.println(name3.endsWith(""));
+	//	System.out.println(name3.equalsIgnoreCase(name1));
+	//	System.out.println(age3.charAt(2));
+		//System.out.println(name1.concat(name3) + "sweet"  + " my mother");
+		
+		
+		/*System.out.println("hello dear");
+		System.out.println("hello madam");
+		Scanner river=new Scanner(System.in);
+		String ring=river.nextLine();
+		
+		System.out.println(" hello " + ring );
+		
+		
+		//Scanner abc=new Scanner (System.in);
+//		String userData= abc.nextLine();
+//		System.out.println(userData);
+//		
+//		System.out.println("what help you need");
+//		
+//		Scanner jolly=new Scanner (System.in);
+//		String cap=jolly.nextLine();
+//		System.out.println(cap);
+//		int h=30;
+//		int a=30;
+//		int b=23;
+//		int c=5;
+//		int e=a-b;
+//		int f=a*b+e;
+//		int d=a+b+c;
+//		 int g=a/c;
+//		
+//		System.out.println(a+b+c);
+//		
+		//Scanner shape=new Scanner(System.in);
+		//String solo=shape.nextLine();
+		//System.out.println(e);
+		
+		//Scanner sweet=new Scanner(System.in);
+		//String ball= sweet.nextLine();
+		
+		//System.out.println(f);
+		
+		/System.out.println(g);
+		//System.out.println(h=a);
+		
+//		System.out.println("hello madam");
+//		Scanner river=new Scanner(System.in);
+//		String ring=river.nextLine();
+//		
+//		System.out.println(" hello " + " river");
+//		
+		//Scanner flow=new Scanner(System.in);
+		//String bcd=flow.nextLine();
+		
+		//System.out.println(" answer = a+b+c  "  + d +  "  right");
+//		
+//			//		Scannerr userInput =new Scanner (System.in);
+		//String userData= userInput.next();
+		//System.out.println(userData);
+		
+				//float l=1234567f;
+       // float n=23;
+		//float m=(float)(l+n);
+		//long o=12345678913l;
+
+	//long p=(long)(o+n);
+	//System.out.println(p);
+	
+		//String x1="25";
+		//int x=23;
+		//int x2=23;
+		//int y=x+x2;
+		//String y="4";
+		//System.out.println(y.isEmpty());
+//		
+//		
+//		
+		//String name =" jyoti ";
+		//System.out.println("name");
+		//System.out.println(name);
+	    //	System.out.println("I am" + x + "years old");
+		//System.out.println( "I am " + name  +  x  +  " years old ");
+       //System.out.println("hello world,welcome to my world");
+		//String name1 = "  prem kumari pandey 007  ";
+		//String name2 = " ";
+		//System.out.println(name1);
+		//System.out.println(name1.length());
+		//System.out.println(name1.charAt(4));
+		//System.out.println(name.concat(" is a beautiful wife "));
+	//	System.out.println(name.concat(" she is boss lady " + " she is " + x + "years old "));
+	//	System.out.println(name.contains("j"));
+		//System.out.println(name1.startsWith("j"));
+		//System.out.println(name.endsWith("NULL"));
+		//System.out.println(name.endsWith("i"));
+		//System.out.println();
+		//System.out.println(name.concat( ".I am beautiful" + "love idli"));
+		
+		//System.out.println(x==(x2));
+		//System.out.println(x1.equals(x));
+		//System.out.println(y);
+		
+		//System.out.println(name.equalsIgnoreCase(" Jyoti "));
+		//System.out.println(name1.indexOf("prem"));
+		//System.out.println(name1.indexOf("k"));
+		//System.out.println(name2.isEmpty());
+		*///System.out.println();//}
+
+//	Scanner userinput=new Scanner(System.in);
+//	
+//	String	color1=userinput.nextLine();
+//	System.out.println(color1);
+//	
+//	    if (color1.contentEquals("red") )
+//	 	
+//	{ 		System.out.println("it is red");   }
+//		else
+//		if	(color1.contentEquals("brown"))
+//			
+//	{		System.out.println("it is brown"); }
+//	    
+//		else 
+//			if 	(color1.contentEquals("green"))
+//			
+//	{		System.out.println("it is green"); }
+//		else
+//			
+//	{       System.out.println("end");        }
+//	}
+//		
+            }
+
+}    
+            
